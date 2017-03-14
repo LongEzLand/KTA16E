a16ee924e12c35251175a8173dd0adc63a1f801c
Tehnikumi Õpilane <opilane@tptlive.ee>
2017-03-14 18:44:38 +0200

    blabla blaa

--- index.php
+++ index.php
@@ -1,26 +1,10 @@
 
 <meta charset = "utf-8">
 
-
-
-<script type="text/javascript">
-    window.onload = function () {
-        (function(){
-            var date = new Date();//paneme aja õigel ajal lukku
-            var time = date.getHours() + ':' +
-                date.getMinutes() + ':'
-                + date.getSeconds();
-                document.getElementsByTagName('div')[0].innerHTML = "Aeg JS'is: "+time;
-            window.setTimeout(arguments.callee, 1000);
-        })();
-    }
-
+<script type="text/javascript" src ="Script.js"></script>
     
-</script>
-
-
-
 <?php
+echo "<div></div>";
 require 'components.php';
 require 'function.php';
 $name = "Tarmo";
